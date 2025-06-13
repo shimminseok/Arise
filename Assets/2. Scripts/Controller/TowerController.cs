0fc82314d37c101b0992b6c75812c39c20c7d614
MINSEOK SHIM <61370379+shimminseok@users.noreply.github.com>
2025-06-13 09:29:52 +0900

    feat : 타워 공격 로직 추가
    타워 공격 로직 추가

--- Assets/2. Scripts/Controller/TowerController.cs
+++ Assets/2. Scripts/Controller/TowerController.cs
@@ -47,23 +47,51 @@ public class TowerController : BaseController<TowerController, TowerState>, IPoo
         return state switch
         {
             TowerState.Idle   => new IdleState(),
-            TowerState.Attack => new AttackState(),
+            TowerState.Attack => new AttackState(StatManager.GetValue(StatType.AttackSpd), StatManager.GetValue(StatType.AttackRange)),
             _                 => null
         };
     }
 
+    public float GetTargetDistance()
+    {
+        if (Target != null && !Target.IsDead)
+        {
+            return Vector3.Distance(transform.position, Target.Transform.position);
+        }
+
+        return Mathf.Infinity;
+    }
+
     public override void FindTarget()
     {
+        var results = new Collider[10];
+        var size    = Physics.OverlapSphereNonAlloc(transform.position, StatManager.GetValue(StatType.AttackRange) * 10, results, LayerMask.GetMask("Enemy"));
+        for (int i = 0; i < size; i++)
+        {
+            if (results[i].TryGetComponent<IDamageable>(out var damageable))
+            {
+                //어택 타입에 따라서 다르게 가져오기
+                //test
+                Target = damageable;
+                break;
+            }
+        }
     }
 
 
-    public void InitFromPool()
+    public void OnSpawnFromPool()
     {
         StatManager.Initialize(towerSO);
+    }
+
+    public void OnReturnToPool()
+    {
     }
 
 
     public void Attack()
     {
+        Debug.Log("Tower Attack");
+        Target.TakeDamage(this);
     }
 }
