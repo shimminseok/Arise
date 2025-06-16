28ed1e3ab608d0e89da1f168c97ed9f7588092e3
rudals4469 <80159080+rudals4469@users.noreply.github.com>
2025-06-16 23:30:19 +0900

    feat : UI 데이터 연동 진행 중

--- Assets/2. Scripts/UI/Buttons/TurretPanelToggleButton.cs
+++ Assets/2. Scripts/UI/Buttons/TurretPanelToggleButton.cs
@@ -17,10 +17,17 @@ public class TurretPanelToggleButton : MonoBehaviour
 
     private void Start()
     {
+        turretPanel.anchoredPosition = hiddenPosition;
+
+        isVisible = false;
+        targetPosition = hiddenPosition;
+
         toggleButton.onClick.AddListener(TogglePanel);
-        targetPosition = shownPosition;
         UpdateButtonText();
+
+        //BuildingPlacer.Instance.ChangeBuilidMode(false);
     }
+
 
     private void Update()
     {
