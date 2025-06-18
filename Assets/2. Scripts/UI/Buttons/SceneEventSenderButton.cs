ec754c4fb963de6eb32735b4ba8b3d197fb9033e
rudals4469 <80159080+rudals4469@users.noreply.github.com>
2025-06-18 09:58:31 +0900

    feat : 튜토리얼 씬 버그 수정

--- Assets/2. Scripts/UI/Buttons/SceneEventSenderButton.cs
+++ Assets/2. Scripts/UI/Buttons/SceneEventSenderButton.cs
@@ -9,7 +9,8 @@ public class SceneEventSenderButton : MonoBehaviour
     [SerializeField] private string sceneToLoadOverride;
     [SerializeField] private bool reloadCurrentSceneInstead = false;
     [SerializeField] private Button button;
-
+    [SerializeField] private GameObject panelToDisableAfterClick;
+    
     private void Awake()
     {
         button.onClick.AddListener(() =>
@@ -30,6 +31,9 @@ public class SceneEventSenderButton : MonoBehaviour
             }
 
             sceneLoadEvent.Raise(targetScene);
+            
+            if (panelToDisableAfterClick != null)
+                panelToDisableAfterClick.SetActive(false);
         });
     }
 }
